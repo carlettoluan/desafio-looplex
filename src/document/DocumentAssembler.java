b06812eb2f9d338a75c95403a2ddf48d323057dc
Luan Carletto <carlettoluan@gmail.com>
2022-07-17 14:08:12 -0300

    third commit

--- src/document/DocumentAssembler.java
+++ src/document/DocumentAssembler.java
@@ -17,29 +17,28 @@ public class DocumentAssembler {
 		System.out.println("| Document Assembler |");
 		System.out.println("|____________________|");
 		
-		System.out.println("Selecione -1 a qualquer momento para sair.");
+		System.out.println("Selecione 0 a qualquer momento para sair.");
 		System.out.println("");
+		
+		while(!selection.equals(0)); {
 		
 		if (branches.isEmpty()) {
 			System.out.println("Àrvore de documentos está vazia");
-		} else {
-			System.out.println("???");
-		
-		do {
-			System.out.println("Gostaria de criar novo documento?");
-			System.out.println("1. Sim");
-			System.out.println("2. Não");
-			System.out.println("3. Status");
-			
 			selection = scan.nextInt();
-			
-			if(selection == 1) {
+		} else {
+				
+			System.out.println("Gostaria de criar nova branch?");
+			System.out.println("1. Sim");
+			System.out.println("2. Não, mostre o status");
+		
+			selection = scan.nextInt();
+				} 
+		
+		if(selection == 1) {
+			// ${BRANCH}+[0];
 				Branch branch1 = new Branch("Branch1");
-				
-				
+				branches.add(branch1);
 			} else if (selection == 2) {
-				break;
-			} else if (selection == 3) {
 				for(Branch branch : branches) {
 					System.out.println(branches);
 				}
@@ -48,7 +47,7 @@ public class DocumentAssembler {
 				System.out.println("Favor informar valor válido");;
 			}
 				
-		} while(!selection.equals(-1));
+		}
 		
 		
 		/*
@@ -73,7 +72,5 @@ public class DocumentAssembler {
 			System.out.println("1 - Criar nova 'branch'");
 	*/
 		scan.close();
-			}
-		}
-	
-}
+			
+		}}
